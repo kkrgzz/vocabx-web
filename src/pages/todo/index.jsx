555f580587c8c9c53f9915fb0068d0627a6e439a
Kemal Karagöz <52218326+kkrgzz@users.noreply.github.com>
2025-02-22 13:31:32 +0300

    Todo grid values changed

--- src/pages/todo/index.jsx
+++ src/pages/todo/index.jsx
@@ -6,10 +6,10 @@ import ListToDo from './list-todo'
 function ToDo() {
     return (
         <Grid container spacing={2}>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={12}>
                 <AddToDo />
             </Grid>
-            <Grid item xs={12} md={6}>
+            <Grid item xs={12} md={12}>
                 <ListToDo />
             </Grid>
         </Grid>
